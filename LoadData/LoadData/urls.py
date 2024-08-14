f7b19a959b763cfbe4b68f1011be4d55f1d6307c
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-08-13 22:39:53 -0700

    Update urls.py

--- LoadData/LoadData/urls.py
+++ LoadData/LoadData/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('My_app.urls')),
-    path('base/',include('Graphs.urls'))
+    path('api/',include('Graphs.urls'))
 ]
