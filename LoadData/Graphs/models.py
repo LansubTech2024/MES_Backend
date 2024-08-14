7aec204a970f0f31dbd80b848be9e6d77501459b
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-08-13 22:36:12 -0700

    Update models.py

--- LoadData/Graphs/models.py
+++ LoadData/Graphs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class GraphModel(models.Model):
+    chw_in_temp = models.FloatField()
+    chw_out_temp = models.FloatField()
+    cow_in_temp = models.FloatField()
+    cow_out_temp = models.FloatField()
+    
+    # Add any other fields if necessary
+    
+    class Meta:
+        managed = False  # If you don't want Django to manage this table
+        db_table = 'MACHINES_TABLE'  # Replace with your actual table name
+    
+    def __str__(self):
+        return f"CHW In: {self.chw_in_temp}, CHW Out: {self.chw_out_temp}, COW In: {self.cow_in_temp}, COW Out: {self.cow_out_temp}"
