fbd554bf35850f2b4a476de8fa4bd02e4e3011dd
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-08-13 22:38:55 -0700

    Update settings.py

--- LoadData/LoadData/settings.py
+++ LoadData/LoadData/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'My_app',
     'Graphs'
     
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'LoadData.urls'
 
