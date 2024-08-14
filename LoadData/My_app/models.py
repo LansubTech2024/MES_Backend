e69216ccb2531dcbc572ea71e56c9add1868aac0
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-08-13 22:40:49 -0700

    Update models.py

--- LoadData/My_app/models.py
+++ LoadData/My_app/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
 class Machine(models.Model):
-    id = models.AutoField(primary_key=True)
     chw_in_temp = models.FloatField()
     chw_out_temp = models.FloatField()
     cow_in_temp = models.FloatField()
     cow_out_temp = models.FloatField()
-    time = models.TimeField()
 
     class Meta:
-        db_table = 'SCHEMA_NAME.MACHINES_TABLE'
+        db_table = 'MACHINES_TABLE'
 
