0f348d32fa129d882e1c03409571c0a9d8795d52
KeerthiKrish8 <Keerthikak0393@gmail.com>
2024-08-29 00:00:26 +0530

    changes in settings

--- LoadData/LoadData/settings.py
+++ LoadData/LoadData/settings.py
@@ -106,10 +106,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'
+EMAIL_HOST_USER = 'intern1@lansubtechnologies.com'
+EMAIL_HOST_PASSWORD = 'Lansub@1234'
 
-FRONTEND_URL = 'http://your-frontend-url.com'
+FRONTEND_URL = 'http://localhost:5175/'
 
 WSGI_APPLICATION = 'LoadData.wsgi.application'
 
