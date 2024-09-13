19443662bd37e33c9da973674a9704554d52e743
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-09-13 22:36:30 +0530

    Update settings.py

--- LoadData/LoadData/settings.py
+++ LoadData/LoadData/settings.py
@@ -157,8 +157,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mes_database',  # New database name
         'USER': 'mes_user',      # New MySQL user
-        'PASSWORD': 'your_password',  # Password you set
-        'HOST': '88.222.245.134',
+        'PASSWORD': 'password',  # Password you set
+        'HOST': 'localhost',
         'PORT': '3306',
 
         },
